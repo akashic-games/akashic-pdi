c58ff427a8d38db57b7ebbbe4a7674095bdc0833
yu-ogi <yusuke_ogiwara@dwango.co.jp>
2020-02-19 14:30:56 +0900

    Drop PointEvent

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { Looper } from "./Looper";
 export { Platform } from "./Platform";
 export { PlatformEventHandler } from "./PlatformEventHandler";
-export { PointType, PointEvent } from "./PointEvent";
 export { RendererRequirement } from "./RendererRequirement";
