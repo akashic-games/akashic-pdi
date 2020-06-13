a518abbb79c1dfa4600bd2548e6fdb579fc013ef
xnv <xnv@users.noreply.github.com>
2020-06-13 20:49:16 +0900

    export all

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,43 @@
-export { Looper } from "./platform/Looper";
-export { Platform } from "./platform/Platform";
-export { PlatformEventHandler } from "./platform/PlatformEventHandler";
-export { RendererRequirement } from "./platform/RendererRequirement";
+export * from "./commons";
+export * from "./errors";
+
+export * from "./surface/ShaderUniform";
+export * from "./surface/ImageData";
+export * from "./surface/CompositeOperation";
+export * from "./surface/CompositeOperationString";
+export * from "./surface/SurfaceLike";
+export * from "./surface/RendererLike";
+export * from "./surface/ShaderProgramLike";
+
+export * from "./asset/video/VideoPlayerLike";
+export * from "./asset/video/VideoSystemLike";
+export * from "./asset/video/VideoAssetLike";
+export * from "./asset/script/ScriptAssetLike";
+export * from "./asset/script/ModuleLike";
+export * from "./asset/script/ScriptAssetRuntimeValue";
+export * from "./asset/image/ImageAssetHint";
+export * from "./asset/image/ImageAssetLike";
+export * from "./asset/audio/AudioAssetLike";
+export * from "./asset/audio/AudioPlayerLike";
+export * from "./asset/audio/AudioSystemLike";
+export * from "./asset/audio/AudioAssetHint";
+export * from "./asset/text/TextAssetLike";
+export * from "./asset/AssetLoadErrorType";
+export * from "./asset/AssetLike";
+
+export * from "./font/GlyphLike";
+export * from "./font/FontWeightString";
+export * from "./font/FontWeight";
+export * from "./font/FontFamily";
+export * from "./font/SurfaceAtlasLike";
+export * from "./font/GlyphFactoryLike";
+export * from "./font/SurfaceAtlasSlotLike";
+
+export * from "./platform/Platform";
+export * from "./platform/Looper";
+export * from "./platform/PlatformPointEvent";
+export * from "./platform/OperationPluginView";
+export * from "./platform/RendererRequirement";
+export * from "./platform/OperationPluginViewInfo";
+export * from "./platform/ResourceFactoryLike";
+export * from "./platform/PlatformEventHandler";
